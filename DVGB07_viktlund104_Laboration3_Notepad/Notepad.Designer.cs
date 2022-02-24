2ab36349ddf380c63ae5c2c4c128cb3f9f62e29e
Viktor Lundberg <vikkolundberg@gmail.com>
2022-02-24 16:26:17 +0100

    first commit

---
+++ DVGB07_viktlund104_Laboration3_Notepad/Notepad.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace DVGB07_viktlund104_Laboration3_Notepad
+{
+	partial class Notepad
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.statusBar = new System.Windows.Forms.StatusBar();
+			this.richTextBox = new System.Windows.Forms.RichTextBox();
+			this.menuStrip.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip
+			// 
+			this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.fileToolStripMenuItem, this.helpToolStripMenuItem});
+			this.menuStrip.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip.Name = "menuStrip";
+			this.menuStrip.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip.TabIndex = 0;
+			this.menuStrip.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.newToolStripMenuItem, this.openToolStripMenuItem, this.saveToolStripMenuItem, this.saveAsToolStripMenuItem, this.exitToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+			this.fileToolStripMenuItem.Text = "Arkiv";
+			// 
+			// newToolStripMenuItem
+			// 
+			this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+			this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys) ((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+			this.newToolStripMenuItem.Size = new System.Drawing.Size(209, 22);
+			this.newToolStripMenuItem.Text = "Ny";
+			this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys) ((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(209, 22);
+			this.openToolStripMenuItem.Text = "Öppna...";
+			this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+			// 
+			// saveToolStripMenuItem
+			// 
+			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+			this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys) ((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+			this.saveToolStripMenuItem.Size = new System.Drawing.Size(209, 22);
+			this.saveToolStripMenuItem.Text = "Spara";
+			this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+			// 
+			// saveAsToolStripMenuItem
+			// 
+			this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+			this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys) (((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) | System.Windows.Forms.Keys.S)));
+			this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(209, 22);
+			this.saveAsToolStripMenuItem.Text = "Spara som...";
+			this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(209, 22);
+			this.exitToolStripMenuItem.Text = "Avsluta";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.aboutToolStripMenuItem});
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+			this.helpToolStripMenuItem.Text = "Hjälp";
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+			this.aboutToolStripMenuItem.Text = "Om Notepad";
+			this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+			// 
+			// statusBar
+			// 
+			this.statusBar.Location = new System.Drawing.Point(0, 428);
+			this.statusBar.Name = "statusBar";
+			this.statusBar.Size = new System.Drawing.Size(800, 22);
+			this.statusBar.TabIndex = 1;
+			this.statusBar.Text = "TODO: Implement status bar functions";
+			// 
+			// richTextBox
+			// 
+			this.richTextBox.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+			this.richTextBox.Location = new System.Drawing.Point(0, 27);
+			this.richTextBox.Name = "richTextBox";
+			this.richTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+			this.richTextBox.Size = new System.Drawing.Size(800, 402);
+			this.richTextBox.TabIndex = 2;
+			this.richTextBox.Text = "";
+			this.richTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox_KeyPress);
+			// 
+			// Notepad
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.richTextBox);
+			this.Controls.Add(this.statusBar);
+			this.Controls.Add(this.menuStrip);
+			this.MainMenuStrip = this.menuStrip;
+			this.Name = "Notepad";
+			this.Text = "Namnlös";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Notepad_FormClosing);
+			this.menuStrip.ResumeLayout(false);
+			this.menuStrip.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+
+		private System.Windows.Forms.RichTextBox richTextBox;
+
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+
+		private System.Windows.Forms.StatusBar statusBar;
+
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+
+		private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+
+		private System.Windows.Forms.MenuStrip menuStrip;
+
+		#endregion
+	}
+}
---
+++ DVGB07_viktlund104_Laboration3_Notepad/Notepad.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DVGB07_viktlund104_Laboration3_Notepad
+{
+	public partial class Notepad : Form
+	{
+		// Tools
+		private OpenFileDialog load;
+		private SaveFileDialog save;
+		private bool isSaved;
+		private bool startup; // possible to start application and instantly open file without YesNoCancel popup
+		private string filePathExists; // to allow quicksave
+
+		// Constructor
+		public Notepad()
+		{
+			InitializeComponent();
+			richTextBox.Font = new Font("Consolas", 11, FontStyle.Regular); // Set font on launch
+			isSaved = false; // initialize
+			startup = true; // first start
+			filePathExists = ""; // initialize to avoid null error when comparing
+		}
+
+		private void newToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			// Check if file is saved and we are not on a fresh start
+			if (isSaved == false && startup == false)
+			{
+				var result = MessageBox.Show("Vill du spara först?", "Notepad", MessageBoxButtons.YesNoCancel,
+					MessageBoxIcon.Stop);
+				if (result == DialogResult.Yes)
+				{
+					SaveAs(false); // Send to save method
+				}
+				else if (result == DialogResult.No)
+				{
+					Clear(); // Clear
+				}
+				else
+				{
+					return; // Cancel, we don't wanna run rest of the code
+				}
+			}
+			else
+			{
+				// File was already saved, clear
+				Clear();
+			}
+		}
+
+		private void openToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (isSaved == false && startup == false)
+			{
+				var result = MessageBox.Show("Vill du spara först?", "Notepad", MessageBoxButtons.YesNoCancel,
+					MessageBoxIcon.Stop);
+				if (result == DialogResult.Yes)
+				{
+					SaveAs(false); // Send to save method
+				}
+				else if (result == DialogResult.No)
+				{
+					Open(); // Send to open method
+				}
+				else
+				{
+					return; // Cancel, we don't wanna run rest of the code
+				}
+			}
+			else
+			{
+				Open();
+			}
+		}
+
+		private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			// if this statement is true, we have opened an existing file or have already saved an file
+			// so we just wanna write to the same path again
+			if (this.Text == Path.GetFileName(filePathExists))
+			{
+				QuickSave(false);
+			}
+			// In this case, we have to open the save as dialog
+			else
+			{
+				SaveAs(false);
+			}
+		}
+		
+
+
+		private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			SaveAs(false);
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Application.Exit(); // To force trigger FormClosing event
+		}
+
+		private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Notepad v1.0\nFree to use\nBy Viktor Lundberg\n2022-02-24", "About", MessageBoxButtons.OK,
+				MessageBoxIcon.Information);
+		}
+
+		private void SaveAs(bool quitSave)
+		{
+			save = new SaveFileDialog();
+			save.Filter = "Text Files (*.txt)|*.txt|All Files (*)|*";
+			var result = save.ShowDialog();
+
+			if (result == DialogResult.OK)
+			{
+				try
+				{
+					File.WriteAllText(save.FileName, richTextBox.Text);
+					this.Text = Path.GetFileName(save.FileName);
+					filePathExists = save.FileName;
+					isSaved = true;
+					statusBar.Text = isSaved.ToString();
+
+					if (quitSave)
+					{
+						Application.Exit();
+					}
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show($"$Error: {ex.Message}", "Error", MessageBoxButtons.OK,
+						MessageBoxIcon.Error);
+				}
+			}
+		}
+		
+		private void QuickSave(bool quitSave)
+		{
+			try
+			{
+				File.WriteAllText(filePathExists, richTextBox.Text);
+				isSaved = true;
+				statusBar.Text = isSaved.ToString();
+
+				if (quitSave)
+				{
+					Application.Exit();
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"$Error: {ex.Message}", "Error", MessageBoxButtons.OK,
+					MessageBoxIcon.Error);
+			}
+		}
+
+		private void Open()
+		{
+			load = new OpenFileDialog();
+			load.Filter = "Text Files (*.txt)|*.txt|All Files (*)|*";
+			var result = load.ShowDialog();
+
+			if (result == DialogResult.OK)
+			{
+				try
+				{
+					richTextBox.Text = File.ReadAllText(load.FileName);
+					this.Text = Path.GetFileName(load.FileName);
+					filePathExists = load.FileName;
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show($"$Error: {ex.Message}", "Error", MessageBoxButtons.OK,
+						MessageBoxIcon.Error);
+				}
+			}
+		}
+
+		private void Clear()
+		{
+			// Clear text and set default title
+			richTextBox.Text = "";
+			this.Text = "Namnlös";
+			isSaved = false;
+			startup = true;
+		}
+
+
+		private void richTextBox_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			isSaved = false;
+			startup = false;
+			statusBar.Text = isSaved.ToString();
+		}
+
+		private void Exit()
+		{
+			if (isSaved == false && startup == false)
+			{
+				var result = MessageBox.Show("Vill du spara först?", "Notepad", MessageBoxButtons.YesNoCancel,
+					MessageBoxIcon.Stop);
+				if (result == DialogResult.Yes)
+				{
+					// Send to save
+					if (this.Text == Path.GetFileName(filePathExists))
+					{
+						QuickSave(true);
+					}
+					else
+					{
+						SaveAs(true);
+					}
+				}
+				else if (result == DialogResult.No)
+				{
+					Application.Exit();
+				}
+				else
+				{
+					return; // Cancel, we don't wanna run rest of the code
+				}
+			}
+			else
+			{
+				Application.Exit();
+			}
+		}
+
+		// Using this event we will catch user clicks off the X button as well as menu exits
+		private void Notepad_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			Exit();
+		}
+	}
+}
